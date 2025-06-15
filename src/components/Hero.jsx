bae5d9485362883467c36045644541e1d70eac7c
joju231 <86151434+joju231@users.noreply.github.com>
2025-06-15 14:51:25 +0200

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section 
       id="home" 
-      className="relative w-full h-screen flex items-center justify-center bg-black overflow-hidden"
+      className="relative w-full h-screen flex items-center justify-center bg-black overflow-hidden border-[#030103]"
     >
       {/* Background Image */}
       <div className="absolute inset-0 z-0 scale-in">
