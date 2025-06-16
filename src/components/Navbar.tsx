2e7b983f9bcfcf9c14fc9ecffea44c9e3d247bcc
joju231 <86151434+joju231@users.noreply.github.com>
2025-06-16 17:18:46 +0200

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar: React.FC = () => {
             <Link
               to="/about"
               className={`text-sm font-medium transition-colors font-secondary ${
-                location.pathname === '/' 
+                location.pathname === '/about' 
                   ? 'text-red-500' 
                   : 'text-white/90 hover:text-white'
               }`}
